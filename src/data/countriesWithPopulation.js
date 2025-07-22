e1672e4048da5b4481030089758eaacc7b91ac91
Erwan Rivault <rivaulterwan@gmail.com>
2025-07-22 10:43:23 +0100

    change threshold to countries > 300k pop

--- src/data/countriesWithPopulation.js
+++ src/data/countriesWithPopulation.js
@@ -1,7 +1,7 @@
 import populationData from 'country-json/src/country-by-population.json';
 
 export const COUNTRIES = populationData
-  .filter(({ population }) => population >= 100000)
+  .filter(({ population }) => population >= 300000)
   .map(({ country, population }) => ({
     name: country,
     population: +population,
