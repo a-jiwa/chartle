2ab938a446a940f60ecabca94741a77855dd2c5f
adnaanjiwa <adnaanjiwa@gmail.com>
2025-08-30 15:05:11 +0100

    Resolved Palestine bug. Palestine was being included in availableCountries because it appeared in the dataset, even though it had no data. Our validation system was treating Palestine as valid because it was in the availableCountries list. Palestine should've been filtered out by the data completeness and population thresholds but wasn't. setAvailableCountries(allCountries) changed to setAvailableCountries(populationFilteredCountries) to fix.

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -132,7 +132,7 @@ export default function Chart({
                 new Set(standardized.map(d => d.Country))
             );
             
-            setAvailableCountries(allCountries);
+            setAvailableCountries(populationFilteredCountries);
                     });
     }, [csvUrl]);
 
